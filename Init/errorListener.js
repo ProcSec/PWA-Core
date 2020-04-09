039f55d0ee3e1079cd66cd8423cd68097bcab6de
Sominemo <me@sominemo.com>
2020-04-09 17:05:44 +0300

    New way to receive reports from DOM

--- Init/errorListener.js
+++ Init/errorListener.js
@@ -8,3 +8,9 @@ window.addEventListener("error", (e) => {
 window.addEventListener("unhandledrejection", (e) => {
     Report.saveToDB("Error (in promise) caught", errorToObject(e.reason))
 })
+
+window.addEventListener("DOMController-Report", (e) => {
+    if (e.detail.type === "log") Report.write(...e.detail.data)
+    if (e.detail.type === "warn") Report.warn(...e.detail.data)
+    if (e.detail.type === "error") Report.error(...e.detail.data)
+})
